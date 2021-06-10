78e385c1de8540b3b539f42ac769eafd81ad802e
jo-teixeira <joao.teixeira@mercadolivre.com>
2021-06-10 13:25:02 -0300

    spring project created

---
+++ src/test/java/com/mercadolivre/DesafioQuality/DesafioQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.DesafioQuality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
